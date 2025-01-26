a6799bc7097be7fced07590247e8a04c3e66b5ac
ddoubledamage <47157358+ddoubledamage@users.noreply.github.com>
2025-01-26 21:15:44 +0300

    Add status & delete method

--- src/app.js
+++ src/app.js
@@ -12,14 +12,18 @@ export const initApp = async () => {
             const ticketEl = document.createElement('div');
             ticketEl.className = 'ticket';
             ticketEl.innerHTML = `
-        <span class="ticket-details" data-id="${ticket.id}">${ticket.name}</span>
         <div>
-          <button class="edit-ticket" data-id="${ticket.id}">✎</button>
-          <button class="delete-ticket" data-id="${ticket.id}">✖</button>
+            <input type="checkbox" class="ticket-checkbox" data-id="${ticket.id}" ${ticket.status ? 'checked' : ''}> 
+            <span class="ticket-details" data-id="${ticket.id}">${ticket.name}</span>
         </div>
-      `;
+        <div>
+            <button class="edit-ticket" data-id="${ticket.id}">✎</button>
+            <button class="delete-ticket" data-id="${ticket.id}">✖</button>
+        </div>
+    `;
             ticketsContainer.appendChild(ticketEl);
         });
+
 
         document.querySelectorAll('.ticket-details').forEach(el => {
             el.addEventListener('click', async (e) => {
@@ -38,6 +42,30 @@ export const initApp = async () => {
                 }
             });
         });
+
+        document.querySelectorAll('.edit-ticket').forEach(el => {
+            el.addEventListener('click', async (e) => {
+                const id = e.target.dataset.id;
+                const ticket = await API.getTicketById(id);
+                const newName = prompt ('Введите новое название тикета', ticket.name);
+                const newDescription = prompt ('Введите новое описание тикета', ticket.description);
+
+                if (newName) {
+                    await API.updateTicket (id, {name: newName, description: newDescription});
+                    renderTickets();
+                }
+            })
+        });
+
+        document.querySelectorAll('.ticket-checkbox').forEach(el => {
+            el.addEventListener('change', async (e) =>{
+                const id = e.target.dataset.id;
+                const status = e.target.checked;
+
+                await API.updateTicket(id, {status});
+                renderTickets();
+            })
+        })
     };
 
     document.body.addEventListener('click', async (e) => {
